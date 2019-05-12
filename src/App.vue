0c0592c17062e44db456a15871bb3bcd3faa1598
yuesheng9527 <1294575751@qq.com>
2019-05-12 22:18:04 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -8,26 +8,7 @@
   </div>
 </template>
 
-<script>
-  document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-    WeixinJSBridge.call('hideToolbar');
-  });
-</script>
-<script>
-if(!$store.state.tag){
-  let u = navigator.userAgent;
-  let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
-  let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-	//alert('是否是Android：'+isAndroid);
-	//alert('是否是iOS：'+isiOS);
-	if(isAndroid) {
-		window.history.pushState({}, "title", "#");
-  } 
-  if(isiOS) {
-		window.history.pushState({}, "title", "#");
-	}
-}
-</script>
+
 <script>
 export default {
   mounted() {
