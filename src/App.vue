4e6f54da1f236b564b6d52aff550d553adddc23c
yuesheng9527 <1294575751@qq.com>
2019-05-13 16:48:07 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -12,28 +12,45 @@
 <script>
 export default {
   created() {
-    if(!this.$store.state.tag){
-      let isIOS= !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
-      console.log(isIOS)
-      if(isIOS){
-        window.history.pushState(null,null, '');
-        window.history.pushState(null,null, '');
-        window.history.pushState(null,null, '');
-        window.history.pushState(null,null, '');
+    if (!this.$store.state.tag) {
+      let isIOS = !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+      console.log(isIOS);
+      if (isIOS) {
+        // window.history.pushState(null,null, '');
+        a();
+        function a () {
+          pushHistory();
+          window.addEventListener(
+            "popstate",
+            function(e) {
+              WeixinJSBridge.invoke("closeWindow", {}, function(res) {});
+            },
+            false
+          );
+
+          function pushHistory() {
+            var state = {
+              title: "title",
+              url: "#"
+            };
+            window.history.pushState(state, "title", "#");
+          }
+        }
       }
     }
   },
   mounted() {
-    if(this.$store.state.tag){
-      document.getElementById("agg").style.minWidth="1280px";
-      document.getElementById("agg").style.minHeight="768px";
+    if (this.$store.state.tag) {
+      document.getElementById("agg").style.minWidth = "1280px";
+      document.getElementById("agg").style.minHeight = "768px";
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-#app2,#agg {
+#app2,
+#agg {
   width: 100%;
   height: 100%;
   background-color: #f5efeff1;
