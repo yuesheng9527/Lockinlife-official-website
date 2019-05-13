2110643f4b840d7fa163e16b7e3564ebf65f3feb
yuesheng9527 <1294575751@qq.com>
2019-05-13 14:48:47 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,15 @@
 <script>
 export default {
   mounted() {
-    $("body").css("height",document.body.clientHeight+"px");
-    // if(!flag){
-    //   let isIOS= !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
-    //   if(isIOS){
-    //     window.history.pushState(null,null, '');
-    //     this,router.go(0);
-    //   }
-    // }
+    if(!this.$store.state.tag){
+      let isIOS= !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
+      if(isIOS){
+        window.history.pushState(null,null, '');
+        window.history.pushState(null,null, '');
+        window.history.pushState(null,null, '');
+        window.history.pushState(null,null, '');
+      }
+    }
     if(this.$store.state.tag){
       document.getElementById("agg").style.minWidth="1280px";
       document.getElementById("agg").style.minHeight="768px";
