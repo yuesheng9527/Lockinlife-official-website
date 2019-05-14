518f68c3ea1034e954fd6b4d4365f084f490e704
yuesheng9527 <1294575751@qq.com>
2019-05-14 11:06:43 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -487,13 +487,13 @@ export default {
       if (!this.$store.state.tag) {
         let pw = new pageSwitch("index_div", {
           duration: 1000, //int 页面过渡时间
-          direction: 1, //int 页面切换方向，0横向，1纵向
+          direction: 0, //int 页面切换方向，0横向，1纵向
           start: 0, //int 默认显示页面
           loop: false, //bool 是否循环切换
           ease: "ease", //string|function 过渡曲线动画，详见下方说明
           transition: "flip3dY", //string|function转场方式，详见下方说明
           freeze: false, //bool 是否冻结页面（冻结后不可响应用户操作，可以通过 `.freeze(false)` 方法来解冻）
-          mouse: false, //bool 是否启用鼠标拖拽
+          mouse: true, //bool 是否启用鼠标拖拽
           mousewheel: true, //bool 是否启用鼠标滚轮切换
           arrowkey: true, //bool 是否启用键盘方向切换
           autoplay: false, //bool 是否自动播放幻灯 新增
