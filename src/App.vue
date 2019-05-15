06316a827507622956f4a018d09626638f5a15dc
yuesheng9527 <1294575751@qq.com>
2019-05-15 10:26:23 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,6 @@
 export default {
   mounted() {
     if(!this.$store.state.tag){
-      // document.body.addEventListener('touchmove', function (e) {
-      //   e.preventDefault() // 阻止默认的处理方式(阻止下拉滑动的效果)
-      // }, {passive: false}) // passive 参数不能省略，用来兼容ios和android
-
       let isIOS= !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
       if(isIOS){
         let ua = navigator.userAgent.toLowerCase();//获取判断用的对象
