d1968e118278f6ca400f0cfebb832750cf7c64d0
yuesheng9527 <1294575751@qq.com>
2019-05-17 09:21:26 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -477,7 +477,7 @@ export default {
           duration: 1000, //int 页面过渡时间
           direction: 0, //int 页面切换方向，0横向，1纵向
           start: 0, //int 默认显示页面
-          loop: true, //bool 是否循环切换
+          loop: false, //bool 是否循环切换
           ease: "ease", //string|function 过渡曲线动画，详见下方说明
           transition: "flip3dY", //string|function转场方式，详见下方说明
           freeze: false, //bool 是否冻结页面（冻结后不可响应用户操作，可以通过 `.freeze(false)` 方法来解冻）
